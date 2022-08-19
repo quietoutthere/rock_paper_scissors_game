e935293d0f32df0983492a4f4374aed460389417
Evan Oyster <oyster.evan@gmail.com>
2022-08-19 08:46:15 -0400

    images added and java updates

---
+++ java/game.js
@@ -0,0 +1,11 @@
+const clickRock = document.getElementById("rock")
+const clickPaper = document.getElementById("paper")
+const clickScissors = document.getElementById("scissors")
+
+clickRock.addEventListener("click", addMessage)
+
+function addMessage() {
+    alert("hellyea")
+}
+
+console.log("hellyea")
