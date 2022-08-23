e01f4133d64781e2856cc73db9505982705c79df
Evan Oyster <oyster.evan@gmail.com>
2022-08-23 10:48:21 -0400

    paper beats rock

--- java/main.js
+++ java/main.js
@@ -1,37 +1,67 @@
-const clicks = document.getElementById("icons")
+const clicks = document.getElementsByClassName("icons")
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
 const returnScore = document.getElementById("score")
 const returnUser = document.getElementById("userChoice")
 const returnComputer = document.getElementById("computerChoice")
 const returnWinner = document.getElementById("winner")
 const choice = ["Rock", "Paper", "Scissors"]
+const y = Math.floor(Math.random()*3);
+
+const choseRock = rock.addEventListener("click", versusRock)
+const chosePaper =paper.addEventListener("click", versusPaper)
+const choseScissors = scissors.addEventListener("click", versusScissors)
+//const displayChoice = clicks.addEventListener("click", )
 
 
-let x = Math.floor(Math.random()*3);
-let y = Math.floor(Math.random()*3);
-let scoreKeep = `(User) ${choice[x]} - ${choice[y]} (Computer)`; 
-let userWon = `The User Chose: ${choice[x]}`;
+
+
+let scoreKeep = `(User)  - ${choice[y]} (Computer)`; 
 let computerWon = `The Computer Chose: ${choice[y]}`;
 
-console.log(scoreKeep)
-clicks.addEventListener("click", addData)
+let results = 
+    returnScore.textContent = scoreKeep;
+    returnComputer.textContent = computerWon;   
 
-function addData() {
-    returnScore.textContent = scoreKeep
-    returnUser.textContent= userWon
-    returnComputer.textContent = computerWon
-    returnWinner.textContent = winner(x,y)
-}
-
-function winner() {
-    if (x > y) {
-        return "The User Won!";
-    } if (x < y) {
-        return "The computer Won!";
-    } if (x = y) {
-        return "It was a tie"
+function versusRock() {
+    results
+    returnWinner.textContent = rockResult();
+        function rockResult() {
+        if (y === 0) {
+            return "It's a tie";
+        } else if (y === 1) {
+            return "You lose";
+        } else if (y === 2) {
+            return "You win";
+        }
     }
 }
 
-function tally()
-    let a = "Noice";
-    return a
+function versusPaper() {
+    results
+    returnWinner.textContent = paperResult()
+        function paperResult() {
+            if (y=== 0) {
+                return "You win";
+            } else if (y===1) {
+                return "It's a tie";
+            } else if (y===2) {
+                return "You lose";
+            }
+        }
+}
+
+function versusScissors() {
+    results
+    returnWinner.textContent = scissorsResult()
+        function scissorsResult() {
+            if (y=== 0) {
+                return "You lose";
+            } else if (y===1) {
+                return "You win";
+            } else if (y===2) {
+                return "It's a tie";
+            }
+        }
+}
