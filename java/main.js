0f002d8798f2ad8e714984615411f938f7f035b7
Evan Oyster <oyster.evan@gmail.com>
2022-08-27 08:09:33 -0400

    super stuck

--- java/main.js
+++ java/main.js
@@ -1,73 +1,79 @@
-const clicks = document.getElementsByClassName("icons")
+const click = document.getElementById("icons")
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
 let returnScore = document.getElementById("score")
 let returnComputer = document.getElementById("computerChoice")
 let returnWinner = document.getElementById("winner")
-const choice = ["Rock", "Paper", "Scissors"]
-const y = Math.floor(Math.random()*3);
-let x=1
+let y = Math.floor(Math.random()*3)
+let choice = ["Rock", "Paper", "Scissors"]
 
+//let computerScore = 0 
+//let userScore = 0
+
+console.log(refresh())
 
 const choseRock = rock.addEventListener("click", versusRock)
-const chosePaper =paper.addEventListener("click", versusPaper)
+const chosePaper = paper.addEventListener("click", versusPaper)
 const choseScissors = scissors.addEventListener("click", versusScissors)
 
-console.log(choice[x])
-console.log()
+const computerChoice = click.addEventListener("click", refresh)
 
-returnScore.textContent = `(User) ${tally(rockResult(), paperResult(), scissorsResult())}  - (Computer)`; 
-      
+function refresh() {
+    return choice[y]
+
+}
 
 function versusRock() {
     returnComputer.textContent = `The Computer Chose: ${choice[y]}`; 
     returnWinner.textContent = rockResult();
-        function rockResult() {
-        if (y === 0) {
-            return "It's a tie";
-        } else if (y === 1) {
-            return "You lose";
-        } else if (y === 2) {
-            return "You win";
-        }
-    }
 }
 
 function versusPaper() {
     returnComputer.textContent = `The Computer Chose: ${choice[y]}`; 
     returnWinner.textContent = paperResult()
-        function paperResult() {
-            if (y=== 0) {
-                return "You win";
-            } else if (y===1) {
-                return "It's a tie";
-            } else if (y===2) {
-                return "You lose";
-            }
-        }
 }
 
 function versusScissors() {
     returnComputer.textContent = `The Computer Chose: ${choice[y]}`; 
     returnWinner.textContent = scissorsResult()
-        function scissorsResult() {
-            if (y=== 0) {
-                return "You lose";
-            } else if (y===1) {
-                return "You win";
-            } else if (y===2) {
-                return "It's a tie";
-            }
-        }
+}
+
+function scissorsResult() {
+    if (y === 0) {
+        return "You lose";
+    } else if (y === 1) {
+        return "You win";
+    } else if (y === 2) {
+        return "It's a tie";
+    }
+}
+
+function rockResult() {
+    if (y === 0) {
+        return "It's a tie";
+    } else if (y === 1) {
+        return "You lose";
+    } else if (y === 2) {
+        return "You win";
+    }
+}
+
+function paperResult() {
+    if (y=== 0) {
+        return "You win";
+    } else if (y===1) {
+        return "It's a tie";
+    } else if (y===2) {
+        return "You lose";
+    }
 }
 
 function tally(a,b,c) {
-    if (a === "you win") {
-        return x++;
-    } else if (b === "you win") {
-        return x++;
-    } else if (c === "you win") {
-        return x++;
+    if (a==="You win" | b=== "You win" | c==="You win") {
+        return userScore++;
+    } else if (a==="You lose" | b==="You lose" | c=== "You lose") {
+        return computerScore++;
     }
 }
+
